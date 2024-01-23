328fef9cd9a3cf0d0be8716970a3522967adf9eb
ustav <gustavobreasy@gmail.com>
2024-01-23 17:42:11 -0300

    add: better createDiv() with image and styles.

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ const options = {
     method: 'GET',
     headers: {
         accept: 'application/json',
-        Authorization: 'Bearer CHANGE YOUR BEARER!'
+        Authorization: 'Bearer CHANGE BEARER HERE'
     }
 };
 
@@ -11,14 +11,19 @@ const url = `https://api.themoviedb.org/3/movie/`;
 let countMovies = 0;
 let pagenumber = 1;
 
-function createDiv(count, elementTitle, elementVoteAverage, insertBeforeID) {
-    const topRatedDiv = document.createElement("div");
-    const topRatedDivContent = document.createTextNode(`${count} ${elementTitle}, rating: ${(elementVoteAverage).toFixed(1)}`);
-    topRatedDiv.appendChild(topRatedDivContent);
-    const currentDiv = document.getElementById(insertBeforeID);
-    document.body.insertBefore(topRatedDiv, currentDiv);
-}
+function createDiv(count, elementTitle, elementImage, elementVoteAverage, insertBeforeID) {
 
+    const div = document.createElement('div');
+    div.className = 'movie';
+    div.innerHTML = `
+        <div id="search-div-inner">
+         <h3>${elementTitle}</h3>
+         <img src="https://image.tmdb.org/t/p/w500/${elementImage}">
+          <h3>${count}rd, Rating: ${(elementVoteAverage).toFixed(1)}</h3>
+        </div>
+    `;
+    document.getElementById(insertBeforeID).appendChild(div);
+}
 
 
 function getTopRated() {
@@ -49,11 +54,11 @@ function getTopRated() {
                 }
 
             }
-
+            console.log(topdata.results)
             topdata.results.forEach(element => {
                 countMovies++
 
-                createDiv(countMovies, element.title, element.vote_average, "div1")
+                createDiv(countMovies, element.title, element.poster_path, element.vote_average, "top-rated-div")
             })
         })
         .catch(err => console.error(err));
@@ -84,7 +89,7 @@ function searchFunction() {
             let pagesLeft = topdata.total_pages - topdata.page
 
             let pagesLeftVisor = document.getElementById('pages-left-search')
-            
+
 
 
             if (pagesLeft > 0) {
@@ -108,9 +113,9 @@ function searchFunction() {
             if (topdata.results.length > 0) {
                 topdata.results.forEach(element => {
                     countSearchedMovies++;
-                    createDiv(countSearchedMovies, element.title, element.vote_average, "search-div")
+                    createDiv(countSearchedMovies, element.title, element.poster_path, element.vote_average, "search-div")
                 })
-                
+
             } else {
                 alert('No corresponding results were found!')
             }
