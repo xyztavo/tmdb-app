3e0c77e1fd0860c83ba0c6d211888cd08cf805ea
ustav <gustavobreasy@gmail.com>
2024-01-25 23:43:08 -0300

    add: responsive, no movie banner placeholder.

--- src/script.js
+++ src/script.js
@@ -2,123 +2,162 @@ const options = {
     method: 'GET',
     headers: {
         accept: 'application/json',
-        Authorization: 'Bearer YOUR BEARER HERE!'
+        Authorization: 'Bearer YOUR BEARER HERE'
     }
 };
 
 const url = `https://api.themoviedb.org/3/movie/`;
 
-let countMovies = 0;
-let pagenumber = 1;
 
+// create movie component with necessary params:
 function createDiv(count, elementTitle, elementImage, elementVoteAverage, insertBeforeID) {
     const div = document.createElement('div');
     div.className = 'movie';
     div.innerHTML = `
         <div id="search-div-inner">
-         <h3>${elementTitle}</h3>
-         <img src="https://image.tmdb.org/t/p/w500/${elementImage}">
-          <h3>${count}rd, Rating: ${(elementVoteAverage).toFixed(1)}</h3>
+         <h3 class="movie-title">${elementTitle}</h3>
+         <img class="movie-img" src="${elementImage}">
+          <h3 class="ratings">${count}rd, Rating:   ${(elementVoteAverage).toFixed(1)} <ion-icon name="star-outline"></ion-icon></h3>
         </div>
     `;
     document.getElementById(insertBeforeID).appendChild(div);
 }
+// todo: get user language
+const languageButton = document.getElementById("language-button")
+let language = ''
+// language = 'language=pt-br'
 
 
-function getTopRated() {
-    fetch(`${url}top_rated?page=${pagenumber}`, options)
-        .then(response => response.json())
-        .then(topdata => {
-            const loadMoreButton = document.getElementById("load-more-button")
+// variables that will be used in getTopRated 
 
+let countMovies = 0;
+let pagenumber = 1;
 
-            let pagesLeft = topdata.total_pages - topdata.page
+// fetch top rated 
+    function getTopRated() {
+        fetch(`${url}top_rated?${language}page=${pagenumber}`, options)
+            .then(response => response.json())
+            .then(topdata => {
+                const loadMoreButton = document.getElementById("load-more-button")
+                let pagesLeft = topdata.total_pages - topdata.page
 
-            if (pagesLeft > 0) {
+                // if pages left greater than 0,
+                if (pagesLeft > 0) {
+                    // make loadmorebutton appear:
+                    loadMoreButton.style.display = 'flex'
 
-                let pagesLeftVisor = document.getElementById('pages-left-top-rated')
-                pagesLeftVisor.innerHTML = 'theres ' + pagesLeft.toLocaleString() + ' pages left!'
-                console.log('theres ' + pagesLeft + ' more pages on top rated!')
-
-
-                loadMoreButton.onclick = () => {
-                    pagenumber++
-                    getTopRated()
+                    // change pagesleftvisor with the pagesleft number
+                    let pagesLeftVisor = document.getElementById('pages-left-top-rated')
+                    pagesLeftVisor.innerHTML = 'theres ' + pagesLeft.toLocaleString() + ' pages left!'
+                    // loadmorebutton onlclick:
+                    loadMoreButton.onclick = () => {
+                        //adds a page number
+                        pagenumber++
+                        // runs the whole function again.
+                        getTopRated()
+                    }
+                } else {
+                    loadMoreButton.style.display = 'none'
                 }
-
-            } else {
-
-                loadMoreButton.onclick = () => {
-                    alert('Nothing to load more!')
-                }
-
-            }
-            console.log(topdata.results)
-            topdata.results.forEach(element => {
-                countMovies++
-
-                createDiv(countMovies, element.title, element.poster_path, element.vote_average, "top-rated-div")
-            })
-        })
-        .catch(err => console.error(err));
-}
-getTopRated()
-
-
-
-let test = 1;
-let pagenumberload = 1;
-
-
-let countSearchedMovies = 0;
-let searchPage = 1;
-function searchFunction() {
-    const searchInput = document.getElementById('search-input');
-
-    let searchUrl = `https://api.themoviedb.org/3/search/movie?query=${searchInput.value}&top_rated&page=${searchPage}`
-
-    fetch(searchUrl, options)
-        .then(response => response.json())
-        .then(topdata => {
-            searchMoreButton = document.getElementById('search-more')
-
-
-
-            let pagesLeft = topdata.total_pages - topdata.page
-
-            let pagesLeftVisor = document.getElementById('pages-left-search')
-
-
-
-            if (pagesLeft > 0) {
-
-                console.log('theres ' + pagesLeft + ' more pages on top rated!')
-                pagesLeftVisor.innerHTML = 'theres ' + pagesLeft.toLocaleString() + ' pages left!'
-
-                searchMoreButton.onclick = () => {
-                    searchPage++
-                    searchFunction()
-                }
-
-
-
-            } else {
-                pagesLeftVisor.innerHTML = 'theres no pages left!'
-                searchMoreButton.onclick = () => {
-                    alert('Nothing to load more!')
-                }
-            }
-
-            if (topdata.results.length > 0) {
-                document.getElementById("search-div").innerHTML = ''
+                // use the component we created for each array result with the params:
                 topdata.results.forEach(element => {
+                    function imageExists() {
+                        if (element.poster_path) {
+                            return `https://image.tmdb.org/t/p/w500/${element.poster_path}`
+                        } else {
+                            return './assets/default-banner-img.png'
+                        }
+                    }
+
+                    countMovies++
+                    createDiv(countMovies, element.title, imageExists(), element.vote_average, "top-rated-div")
+                })
+            })
+            .catch(err => console.error(err));
+    }
+    getTopRated()
+
+    //variables that will be used in search function
+    let countSearchedMovies = 0;
+    let searchPage = 1;
+
+    const searchButton = document.getElementById('search')
+
+    searchButton.onclick = () => {
+        document.getElementById("search-div").innerHTML = ''
+        searchFunction()
+        countSearchedMovies = 0;
+    }
+
+    function searchFunction() {
+        const searchInput = document.getElementById('search-input');
+        const searchMoreButton = document.getElementById('search-more')
+
+        let searchUrl = `https://api.themoviedb.org/3/search/movie?query=${searchInput.value}&${language}page=${searchPage}`
+
+        fetch(searchUrl, options)
+            .then(response => response.json())
+            .then(topdata => {
+                let pagesLeft = topdata.total_pages - topdata.page;
+                let pagesLeftVisor = document.getElementById('pages-left-search');
+
+
+
+                // check the pages left greater than 0.
+                if (pagesLeft > 0) {
+                    // if so, show load more button.
+                    searchMoreButton.style.display = "flex";
+
+                    //change the pagesleftvisor (which id is pages-left-search).
+                    pagesLeftVisor.innerHTML = 'theres ' + pagesLeft.toLocaleString() + ' pages left!'
+
+                    //load more button onclick:
+                    searchMoreButton.onclick = () => {
+                        // add to searchPage ( number of the actual page ).
+                        searchPage++
+                        // run this whole function again.
+                        searchFunction()
+                    }
+                    // if its not greater than 0,
+                } else {
+                    // change pagesleftvisor.
+                    pagesLeftVisor.innerHTML = 'theres no pages left!'
+                    // make loadmorebutton invisible.
+                    searchMoreButton.style.display = "none";
+                }
+
+                // use the component we created for each array result with the params:
+                topdata.results.forEach(element => {
+                    function imageExists() {
+                        if (element.poster_path) {
+                            return `https://image.tmdb.org/t/p/w500/${element.poster_path}`
+                        } else {
+                            return './assets/default-banner-img.png'
+                        }
+                    }
+
                     countSearchedMovies++;
-                    createDiv(countSearchedMovies, element.title, element.poster_path, element.vote_average, "search-div")
+                    createDiv(countSearchedMovies, element.title, imageExists(), element.vote_average, "search-div")
+
                 })
 
-            } else {
-                alert('No corresponding results were found!')
-            }
-        })
-        .catch(err => console.error(err));
-}
+
+
+
+
+            })
+            .catch(err => console.error(err));
+    }
+
+    // this makes the default loadmorebutton for searched movies invisible, TODO: change that to somewhere else
+
+
+    //simple script to make pressing enter on a input to click a button
+    const searchInput = document.getElementById('search-input');
+    searchInput.addEventListener("keypress", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            searchButton.click();
+        }
+    });
+
